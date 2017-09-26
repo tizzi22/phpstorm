32bfc147f8c10b49d35faeb40ac1037fc2a590c0
rusavskiy <rusavskiy@gmail.com>
2017-09-26 10:43:50 +0300

    PS-2017.2.4 <v@vv Create markdown.xml, CMS.xml, IntelliLang.xml, project.default.xml, diff.xml, applicationLibraries.xml, Default.xml, file.template.settings.xml, casper.icls, find.xml, SQL Script.sql, code.style.schemes.xml, git.xml, Dart.xml, Text.xml, Default.xml, PHP.xml, JavaScript.xml, laf.xml, Doc.xml, surround.xml, editor.codeinsight.xml, editor.xml, uml.xml, hg.xml, webServers.xml, debugger.xml, path.macros.xml, baseRefactoring.xml, smarty.xml, ide.general.xml, other.xml, PHP Class Doc Comment.php, Zen CSS.xml, xpath.xml, keymap.xml, ui.lnf.xml, php.xml, casper.xml, javaeeExternalResources.xml, remote-servers.xml, watcherDefaultTasks.xml, colors.scheme.xml, html_xml.xml, External Tools.xml, notifications.xml, databaseDrivers.xml, templates.xml, SQL.xml, customization.xml, External Tools.xml, Casper.xml, PHP File Header.php, intentionSettings.xml, web-browsers.xml, proxy.settings.xml, vcs.xml, databaseSettings.xml, usageView.xml

---
+++ fileTemplates/includes/PHP Class Doc Comment.php
@@ -0,0 +1,6 @@
+/**
+ * Class ${NAME}
+ * User: Rusavskiy Vitaliy <rusavskiy.v@bintime.com>
+#if (${NAMESPACE}) * @package ${NAMESPACE}
+#end
+ */
---
+++ fileTemplates/includes/PHP File Header.php
@@ -0,0 +1,6 @@
+ /**
+  * @link http://www.bintime.com
+  * @copyright 2017 (c) Bintime
+  * @author Rusavskiy Vitaliy <rusavskiy.v@bintime.com>
+  * @date ${DATE}
+ */
